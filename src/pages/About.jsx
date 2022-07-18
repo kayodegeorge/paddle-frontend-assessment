640b626bbbb9ab65f91e54e2bfe0430d8389e1dd
kayodegeorge <gkayode75@gmail.com>
2022-07-18 12:30:56 +0100

    made changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { BsArrowRight } from "react-icons/bs";
+import Footer from '../components/Footer';
 
 function About() {
   return (
     <>
       <Navbar />
-      <div className="p-12 space-y-20 text-white">
+      <div className="space-y-20 text-white">
         <section className="flex flex-col space-y-12">
-          <span className="text-sm">ABOUT US</span>
-          <h1 className="text-5xl ml-96 p-20">
-            Built for SaaS <br /> and E-Commerce
-          </h1>
-          <div className="flex justify-end">
+          <div className="flex flex-col items-center my-32">
+            <span className="text-sm md:mr-96 mb-6">ABOUT US</span>
+            <h1 className="text-4xl px-12 md:px-0 md:text-6xl md:ml-32">
+              Built for SaaS <br /> and E-Commerce
+            </h1>
+          </div>
+
+          <div className="flex justify-center px-12 md:px-0 md:justify-end md:mr-32">
             <p className="text-sm w-96">
               Our tools are easy to set up and use with a user friendly
               dashboard that enables you set up, launch, automate and manage
@@ -21,16 +26,16 @@ function About() {
         </section>
 
         <section className="text-center">
-          <h2 className="px-[19rem] py-24 text-4xl text-left">
+          <h2 className="mx-12 md:mx-32 md:pr-44 py-24 text-xl md:text-5xl text-left">
             Metricks was developed because just like you, we needed a product
             that could give us <strong>good value</strong>.
           </h2>
         </section>
 
-        <section className="flex py-24">
-          <div>
+        <section className="flex justify-center mx-12 md:mx-32 my-24">
+          <div className="space-y-3">
             <span>WHY US?</span>
-            <p className="pr-24">
+            <p className="md:pr-24">
               We pride ourselves in oiur ability to innovate and create
               world-class tools that provide reliable and efficient touchpoints
               between advertisers and affiliates.
@@ -39,34 +44,41 @@ function About() {
 
           <div className="space-y-6">
             <span>GROWING WITH YOU</span>
-            <div className="space-y-8 pr-64">
-              <p>
-                Leveraging the best technology, we have developed an all-in-one
-                affiliate marketing tracking software, a genius tool to help you
-                track, automate and optimize your influencer campaigns, all from
-                one dashboard.
-              </p>
-              <p>
-                Our team of experts are constantly brainstorming, testing and
-                developing new solutions with only one thing in mind - your
-                business growth. Your success is our success and by giving you
-                the tools you need to scale, we grow as well.
-              </p>
+            <div className="space-y-8">
+              <div className="w-2/3 space-y-6">
+                <p>
+                  Leveraging the best technology, we have developed an
+                  all-in-one affiliate marketing tracking software, a genius
+                  tool to help you track, automate and optimize your influencer
+                  campaigns, all from one dashboard.
+                </p>
+                <p>
+                  Our team of experts are constantly brainstorming, testing and
+                  developing new solutions with only one thing in mind - your
+                  business growth. Your success is our success and by giving you
+                  the tools you need to scale, we grow as well.
+                </p>
+              </div>
             </div>
           </div>
         </section>
 
-        <section className="space-y-6">
+        <section className="px-12 md:px-32 py-12 space-y-6">
           <h2 className="text-3xl">Got a Question?</h2>
           <p className="text-sm">
             See how Metricks can help your business grow with best affiliate
             marketing tracking software.
           </p>
-          <h4 className="text-sm text-fuchsia-600" href="/">
-            Contact Us
-          </h4>
+          <div className="flex items-center space-x-2 text-fuchsia-600">
+            <h4 className="text-sm" href="/">
+              Contact Us
+            </h4>
+            <BsArrowRight />
+          </div>
         </section>
+        
       </div>
+      <Footer/>
     </>
   )
 }
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,7 @@
 import React, {useState} from 'react'
+// import Navbar from '../components/Navbar'
+import {AiOutlineArrowRight} from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 
 function ContactUs() {
 
@@ -8,10 +11,45 @@ function ContactUs() {
     }
   return (
     <div>
-        <div className={`fixed ${
-        nav ? "right-0" : "right-[-100%]"
-      } top-0 w-[50%] h-full border-r-blue-600 bg-[#1A023D] ease-in-out duration-500 md:hidden`}>
-       
+      
+      <div className="flex lg:w-[50%] lg:ml-[1000px] w-[50%] h-full ease-in-out">
+           
+           <Link to="/">
+           <div className=" bg-white rounded-full h-[70px] p-5 pb-5 m-5 mt-7">
+             <div className="text-blue-700">
+             <AiOutlineArrowRight size={35}/>
+             
+             </div>
+             
+           </div>
+           </Link>
+           <div className="w-full flex flex-col justify-center mx-auto">
+             <h1 className='text-white font-bold text-4xl text-center mt-14'>Hey, we are still in the works,</h1>
+             <h1 className='text-white font-bold text-4xl text-center mt-4'>but you can send us a message!</h1>
+
+             
+             <div className="justify-center">
+               <p className='text-white mt-10 pl-10'>First Name</p>
+             <input type="text" placeholder='Enter your first name' name="" className='m-10 text-gray-700 font-bold p-10 w-[80%] rounded-md border-none' />
+             </div>
+             <div className="justify-center">
+               <p className='text-white mt-10 pl-10'>Last Name</p>
+             <input type="text" placeholder='Enter your Last name' name="" className='m-10 text-gray-700 font-bold p-10 w-[80%] rounded-md border-none' />
+             </div>
+             <div className="justify-center">
+               <p className='text-white mt-10 pl-10'>Email Address</p>
+             <input type="text" placeholder='Enter your Email Address' name="" className='m-10 text-gray-700 font-bold p-10 w-[80%] rounded-md border-none' />
+             </div>
+             <div className="justify-center">
+               <p className='text-white mt-10 pl-10'>Tell us what you need help with:</p>
+             <input type="text" placeholder='Enter a Topic, like: "Channel problem..."' name="" className='m-10 text-gray-700 font-bold p-20 w-[80%] rounded-md border-none' />
+             </div>
+             <div className="">
+               <button className='bg-blue-700 p-8 w-[30%] rounded-full text-white font-bold'>
+                 SEND NOW
+               </button>
+             </div>
+           </div>
       </div>
     </div>
   )
